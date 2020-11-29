6231120e5caa13e38c1f6a5381eb59fbb4117378
ankit.tomar08 <ankit.tomar08@gmail.com>
2020-11-29 13:52:43 -0500

    Added unit test case for RestaurantsViewModel

--- app/src/test/java/ankit/com/sampleapp/view/RestaurantsViewModelTest.kt
+++ app/src/test/java/ankit/com/sampleapp/view/RestaurantsViewModelTest.kt
@@ -1,8 +1,11 @@
 package ankit.com.sampleapp.view
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
-import ankit.com.sampleapp.data.entity.Restaurant
 import ankit.com.sampleapp.domain.DomainTestData
+import ankit.com.sampleapp.domain.model.RestaurantDomainModel
+import ankit.com.sampleapp.domain.usecase.AddToFavoritesUseCase
+import ankit.com.sampleapp.domain.usecase.FilterRestaurantsUseCase
 import ankit.com.sampleapp.domain.usecase.GetRestaurantsUseCase
 import ankit.com.sampleapp.util.UIResponseState
 import com.nhaarman.mockitokotlin2.verify
@@ -11,16 +14,22 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.TestCoroutineScope
 import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.setMain
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TestRule
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
 import org.mockito.junit.MockitoJUnitRunner
+import java.lang.Exception
+import java.text.ParseException
+
 
 /**
  * Created by AnkitSingh on 11/25/20.
@@ -32,39 +41,57 @@ class RestaurantsViewModelTest {
 
     private val testCoroutineScope = TestCoroutineScope(testCoroutineDispatcher)
 
+    @get:Rule
+    val testInstantTaskExecutorRule: TestRule = InstantTaskExecutorRule()
+
     @Mock
     private lateinit var restaurantsUseCase: GetRestaurantsUseCase
 
     @Mock
-    private lateinit var restaurantsListObserver: Observer<List<Restaurant>>
+    private lateinit var addToFavoritesUseCase: AddToFavoritesUseCase
+
+    @Mock
+    private lateinit var filterRestaurantsUseCase: FilterRestaurantsUseCase
+
+    @Mock
+    private lateinit var restaurantsListObserver: Observer<List<RestaurantDomainModel>>
 
     private lateinit var viewModel: RestaurantsViewModel
 
     @Before
     fun setUp() {
         MockitoAnnotations.openMocks(this)
-        //viewModel = RestaurantsViewModel(restaurantsUseCase)
+        viewModel = RestaurantsViewModel(restaurantsUseCase, addToFavoritesUseCase, filterRestaurantsUseCase)
         Dispatchers.setMain(testCoroutineDispatcher)
     }
 
-//    @Test
-//    fun `get restaurants usecase returns list of restaurants success scenario`() = testCoroutineScope.runBlockingTest {
-//        // when
-//        val testData = DomainTestData.getRestaurantsDummyData()
-//        val testFlow = flow {
-//            delay(100)
-//            emit(UIResponseState.Success(testData.t))
-//        }
-//
-//        whenever(restaurantsUseCase.execute()).thenReturn(testFlow)
-//
-//        viewModel.getRestaurantsData()
-//        viewModel.restaurants.observeForever(restaurantsListObserver)
-//
-//        verify(restaurantsUseCase).execute()
-//        verify(restaurantsListObserver).onChanged(testData)
-//        advanceTimeBy(100)
-//
-//        viewModel.restaurants.removeObserver(restaurantsListObserver)
-//    }
+    @Test
+    fun `get restaurants usecase returns list of restaurants success scenario`() = testCoroutineScope.runBlockingTest {
+        // when
+        val testData = DomainTestData.getRestaurantsDummyData()
+        whenever(restaurantsUseCase.execute()).thenReturn(flowOf(UIResponseState.Success(testData)))
+
+        viewModel.getRestaurantsData()
+        viewModel.restaurants.observeForever(restaurantsListObserver)
+
+        verify(restaurantsUseCase).execute()
+        verify(restaurantsListObserver).onChanged(testData)
+
+        viewModel.restaurants.removeObserver(restaurantsListObserver)
+    }
+
+    @Test
+    fun `get restaurants usecase returns Error scenario`() = testCoroutineScope.runBlockingTest {
+        // when
+        val exception = Exception()
+        whenever(restaurantsUseCase.execute()).thenReturn(flowOf(UIResponseState.Error(exception)))
+
+        viewModel.getRestaurantsData()
+        viewModel.restaurants.observeForever(restaurantsListObserver)
+
+        verify(restaurantsUseCase).execute()
+        verify(restaurantsListObserver).onChanged(emptyList())
+
+        viewModel.restaurants.removeObserver(restaurantsListObserver)
+    }
  }
